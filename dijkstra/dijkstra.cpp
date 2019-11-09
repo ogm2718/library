cd5aa1f5f1d8f8e3e280d13a0dc79942c155ac0c
ogm <42830659+ogm2718@users.noreply.github.com>
2019-11-09 23:23:38 +0900

    Update dijkstra.cpp

--- dijkstra/dijkstra.cpp
+++ dijkstra/dijkstra.cpp
@@ -1,29 +1,21 @@
-struct edge { int to, cost};
-typedef pair<int , int> pi;
-typedef vector<edge> Graph;
-vector<int> dist;
+vector<ll> dist;
 int start,goal;
-Graph g[MAX_V];
 
 void dijkstra(){
   priority_queue<pi,vector<pi>,greater<pi> > pq;
-  fill(dist, dist+v,INF);
+  dist.resize(n+1,INF);
   dist[start]=0;
   pq.push(pi(0,start));
-
   while(!que.empty()){
     int d = pq.top().first, v = pq.top().second;
     pq.pop();
-
-    if(v == goal)break;
     if(dist[v] < d)continue;
 
-    for(int i=0; i < (int)g[v].size(); ++i){
-      edge e = g[v][i];
-
-      if(d + e.cost < dist[e.to]){
-        dist[e.to] = d + e.cost;
-        pq.push(pi(dist[e.to],e.to));
+    for(auto e:G[v]){
+      ll to = e.first,cost = e.second;
+      if(d + e.first < dist[e.to]){
+        dist[to] = d + cost;
+        pq.push(pi(dist[to],to));
       }
     }
   }
