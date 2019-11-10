d0581f4832d60ea8e36b77743a7a9d804d56f19a
ogm <42830659+ogm2718@users.noreply.github.com>
2019-11-10 18:43:06 +0900

    Update dijkstra.cpp

--- dijkstra/dijkstra.cpp
+++ dijkstra/dijkstra.cpp
@@ -1,22 +1,22 @@
-vector<ll> dist;
-int start,goal;
-
-void dijkstra(){
-  priority_queue<pi,vector<pi>,greater<pi> > pq;
-  dist.resize(n+1,INF);
-  dist[start]=0;
-  pq.push(pi(0,start));
-  while(!que.empty()){
-    int d = pq.top().first, v = pq.top().second;
-    pq.pop();
-    if(dist[v] < d)continue;
-
-    for(auto e:G[v]){
-      ll to = e.first,cost = e.second;
-      if(d + e.first < dist[e.to]){
-        dist[to] = d + cost;
-        pq.push(pi(dist[to],to));
-      }
+void dijkstra(vector<vector<pair<ll,ll>>> &G, ll n, vector<ll> &dist, ll start) {
+    priority_queue<pi, vector<pi>, greater<pi>> pq;
+    dist[start] = 0;
+    vector<ll> used(n+1,0);
+    pq.push({0, start});
+    while(!pq.empty()) {
+        ll d = pq.top().first, v = pq.top().second;
+        pq.pop();
+        if(used[v])continue;
+        used[v] = 1;
+        if(dist[v]<d){
+            continue;
+        }
+        for(pi e : G[v]) {
+            ll to = e.first, cost = e.second;
+            if(dist[v] + e.second < dist[to]) {
+                dist[to] = dist[v] + cost;
+                pq.push({dist[to], to});
+            }
+        }
     }
-  }
 }
