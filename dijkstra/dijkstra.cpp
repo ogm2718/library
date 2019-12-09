4d446a184785ead215c53a95227ca344ea43475a
ogm <42830659+ogm2718@users.noreply.github.com>
2019-12-09 20:11:31 +0900

    Update dijkstra.cpp

--- dijkstra/dijkstra.cpp
+++ dijkstra/dijkstra.cpp
@@ -1,18 +1,21 @@
-void dijkstra(vector<vector<pair<ll,ll>>> &G, ll n, vector<ll> &dist, ll start) {
-    priority_queue<pi, vector<pi>, greater<pi>> pq;
+template <typename T, typename U>
+void dijkstra(const vector<vector<pair<T, U>>> &G, const int &n, vector<U> &dist, const T &start) {
+    priority_queue<pair<U, T>, vector<pair<U, T>>, greater<pair<U, T>>> pq;
     dist[start] = 0;
-    vector<ll> used(n+1,0);
+    vector<bool> used(n + 1, 0);
     pq.push({0, start});
     while(!pq.empty()) {
-        ll d = pq.top().first, v = pq.top().second;
+        U d = pq.top().first;
+        T v = pq.top().second;
         pq.pop();
-        if(used[v])continue;
+        if(used[v]) continue;
         used[v] = 1;
-        if(dist[v]<d){
+        if(dist[v] < d) {
             continue;
         }
-        for(pi e : G[v]) {
-            ll to = e.first, cost = e.second;
+        for(auto &e : G[v]) {
+            T to = e.first;
+            U cost = e.second;
             if(dist[v] + e.second < dist[to]) {
                 dist[to] = dist[v] + cost;
                 pq.push({dist[to], to});
